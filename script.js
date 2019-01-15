4af0eb30016d3c3f5c3d0999586c66b389bc8ae8
Daniel Cenoz <josedanielc@ad.corp.google.com>
2019-01-15 15:50:46 -0200

    <josedanielc@ad.corp.google.com>

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function(){
    
+    // test if #boton exists and add the eventlistner to it
     if(document.querySelector('#boton')){
         
         document.querySelector('#boton').addEventListener('click', function(){
@@ -11,5 +12,22 @@ document.addEventListener('DOMContentLoaded', function(){
     } else {
         console.log('what???');
     }
-    
+    // end test #boton
+
 });
+
+ //TEST AJAX
+
+ document.addEventListener('DOMContentLoaded', function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.querySelector("#demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
+
+);
+  //End test AJAX
